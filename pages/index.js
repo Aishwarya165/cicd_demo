425136a24f23b55421c38809a0b7f98ba99fd4a1
ajith shah <ajithshah37@gmail.com>
2022-09-22 12:39:17 +0530

    [Ajith] Add Axios

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [career, setCareer] =useState([]);
+  useEffect(() => {
+    axios.get("/Career").then((res) => {
+      setCareer(res.data);
+        console.log(career);
+    })
+  });
+
   return (
     <div className={styles.container}>
-     <p>hi</p>
+      <p>Main Page</p>
     </div>
   )
 }
