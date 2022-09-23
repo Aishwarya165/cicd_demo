842893ab5492592f397a105ec46ebc5421920ea5
ajith shah <ajithshah37@gmail.com>
2022-09-23 15:58:28 +0530

    [Ajith] Hit Cloud Directus Api Success

--- pages/index.js
+++ pages/index.js
@@ -3,61 +3,19 @@ import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const [udata, setData] = useState([])
+  const [dataModel, setData] = useState({ data: [] })
 
   useEffect(() => {
     axios.get('https://pvik3n4r.directus.app/items/blog').then((res) => {
-      setData(res.udata);
-      console.log(udata);
+      setData(res.data);
     })
   });
-
-  const inputObject = {
-    "userList":
-    [{
-      "name": "Bob",
-      "age": 28
-    },{
-      "name": "Tom",
-      "age": 45
-    },{
-      "name": "Alice",
-      "age": 32
-    }]
-  };
-  // console.log(data);
-  // console.log(inputObject);
-  const arr=[
-    {
-      "name":"ajith",
-      "age":12
-    },
-    {
-      "name":"ajith",
-      "age":12
-    },
-    {
-      "name":"ajith",
-      "age":12
-    }
-  ];
-  // console.log(arr);
-  // const output = blog.data.map(blog => blog.name);
-
-
+  
   return (
     <div className={styles.container}>
       <p>Main Page</p>
-      
-      {inputObject.userList.map(({name, age}, index) => {
-        return(
-          <div key={index}>
-            <p>{name}{' '}{age}</p>
-          </div>
-        );
-      })}
-
-      {data.map(({id, title}, index) => {
+    
+      {dataModel.data.map(({id, title}, index) => {
         return(
           <div key={index}>
             <p>{id}{' '}{title}</p>
