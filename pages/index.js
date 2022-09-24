f0e4dccc72ad6ccaa7703cefa05b4474c4154833
ajith shah <ajithshah37@gmail.com>
2022-09-24 14:17:17 +0530

    [Ajith] Add Career and Blog API

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,51 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 export default function Home() {
-  const [dataModel, setData] = useState({ data: [] })
-
+  const [blogData, setBlogData] = useState({ data: [] })
+  const [careerData, setCareerData] = useState({ data: [] })
+  const imageUrl= "https://pvik3n4r.directus.app/assets/"
   useEffect(() => {
     axios.get('https://pvik3n4r.directus.app/items/blog').then((res) => {
-      setData(res.data);
+      setBlogData(res.data);
     })
-  });
-  
+  },[]);
+  useEffect(() => {
+    axios.get('https://pvik3n4r.directus.app/items/career').then((res) => {
+      setCareerData(res.data);
+    })
+  },[]);
   return (
     <div className={styles.container}>
-      <p>Main Page</p>
-    
-      {dataModel.data.map(({id, title}, index) => {
-        return(
-          <div key={index}>
-            <p>{id}{' '}{title}</p>
-          </div>
-        );
-      })}
+
+      <p className={styles.title}>Main Page</p>
+
+      <div className={styles.maincontainer}>
+
+        <div className={styles.blog}>
+          {careerData.data.map(({id, title, discription}, index) => {
+            return(
+              <div className={styles.single} key={index}>
+                <p>{id}{' '}{title}</p>
+                <p dangerouslySetInnerHTML={{__html: discription}}></p>
+              </div>
+            )
+          })}
+        </div>
+
+        <div className={styles.career}>
+          {blogData.data.map(({id, title, image, content}, index) => {
+            return(
+              <div className={styles.single} key={index}>
+                <p>{id}{' '}{title}</p>
+                <img src={process.env.IMAGEURL+{image}} alt/>
+                <p dangerouslySetInnerHTML={{__html: content}}></p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   )
 }
